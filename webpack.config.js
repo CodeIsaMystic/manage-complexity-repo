0f9edcd0aab79b7d54a2ae9763ed659d767a6aa0
Julien Orjollet <oj.professionnel@gmail.com>
2021-07-29 13:39:52 +0200

    reordering files and structure of starter project

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,46 @@
-const path = require('path');
+const path = require("path");
 const postCSSPlugins = [
-  require('postcss-import'),
-  require('postcss-mixins'),
-  require('postcss-simple-vars'),
-  require('postcss-nested'),
-  require('postcss-hexrgba'),
-  require('autoprefixer')
+  require("postcss-import"),
+  require("postcss-mixins"),
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("postcss-hexrgba"),
+  require("autoprefixer"),
 ];
 
 module.exports = {
-  entry: './app/assets/scripts/App.js',
+  entry: "./app/scripts/App.js",
   output: {
-    filename: 'bundled.js',
-    path: path.resolve(__dirname, 'app')
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
   },
   devServer: {
     before: function (app, server) {
-      server._watch('./app/**/*.html');
+      server._watch("./app/**/*.html");
     },
-    contentBase: path.join(__dirname, 'app'),
+    contentBase: path.join(__dirname, "app"),
     hot: true,
     port: 3000,
-    host: '0.0.0.0'
+    host: "localhost",
   },
-  mode: 'development',
+  mode: "development",
   module: {
-    rules: [{
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader?url=false', {
-        loader: 'postcss-loader',
-        options: {
-          plugins: postCSSPlugins
-        }
-      }]
-    }]
-  }
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader?url=false",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: postCSSPlugins,
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
 // add this short code on the rules:use:
 //            'css-loader?url=false',
