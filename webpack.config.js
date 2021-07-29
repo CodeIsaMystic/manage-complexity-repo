3a2ca42928917ae2faf4dfeeb7f0079eef938ca7
Julien Orjollet <oj.professionnel@gmail.com>
2021-07-29 13:29:48 +0200

    init commit -m

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const postCSSPlugins = [
+  require('postcss-import'),
+  require('postcss-mixins'),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('postcss-hexrgba'),
+  require('autoprefixer')
+];
+
+module.exports = {
+  entry: './app/assets/scripts/App.js',
+  output: {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
+  },
+  devServer: {
+    before: function (app, server) {
+      server._watch('./app/**/*.html');
+    },
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader?url=false', {
+        loader: 'postcss-loader',
+        options: {
+          plugins: postCSSPlugins
+        }
+      }]
+    }]
+  }
+};
+// add this short code on the rules:use:
+//            'css-loader?url=false',
+//right after 'style-loader',  and before [loader:]
+// to manage ourself our bg-img on a css file
